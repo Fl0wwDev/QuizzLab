688026d9a97d2b177e64bf39a3c1438471960ac9
Fl0wwDev <mafradin@hotmail.fr>
2025-11-21 14:52:20 +0100

    Refactor Home component to fetch data asynchronously and display correct answer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
+'use client';
+import {useState, useEffect} from "react";
 import { Button } from "@/src/components/ui/button";
+interface Mythologie {
+  id: string;
+  nom: string;
+  definition: string;
+  nomRomain: string;
+}
+export default function Home() {
+  const [data, setData] = useState<Mythologie[]>([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+      const response = await fetch('/api/culture-g');
+      const json = await response.json();
+      setData(json.data);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données:', error);
+      }
+    }
+    fetchData();
+  }, []);
 
-export default async function Home() {
-  const posts = await fetch('http://localhost:3000/api/culture-g').then((res) => res.json());
-  console.log(posts);
+  function findCorrectAnswer(options: Mythologie[], correctId: string): string {
+    const correctOption = options.find(option => option.id === correctId);
+    return correctOption ? correctOption.nomRomain : "Réponse inconnue";
+  }
   return (
-    <Button> salut</Button>
+    <p>{findCorrectAnswer(data, "2b171c84-d568-80e2-ae63-e9f62fcc82c8")}</p>
   );
 }
